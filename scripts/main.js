15784a593e32940304e20d4a9104c037a5439f71
xuefei.shi519 <xuefei.shi519@gmail.com>
2016-05-20 23:52:52 -0400

    shinos commit

--- scripts/main.js
+++ scripts/main.js
@@ -37,3 +37,34 @@ if(!localStorage.getItem('name')) {
 myButton.onclick=function(){
   setName();
 }*/
+
+var imgUrl = 'images/nyan.png';
+//分享链接的链接地址
+var lineLink = 'https://xuefei519.github.io/mirabelle/';
+//分享链接的描述信息
+var descContent = "This page is for Mirabelle";
+//分享链接的标题
+var shareTitle = "Mirabelle's Page";
+//一般为空 就好
+var appid = '';
+//分享给好友
+function shareFriend() {
+    WeixinJSBridge.invoke('sendAppMessage', {
+        "appid": appid,
+        "img_url": imgUrl,
+        "img_width": "640",
+        "img_height": "640",
+        "link": lineLink,
+        "desc": descContent,
+        "title": shareTitle
+    }, function(res) {
+        _report('send_msg', res.err_msg);
+    })
+}
+// 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
+document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+    // 发送给好友
+    WeixinJSBridge.on('menu:share:appmessage', function(argv) {
+        shareFriend();
+    });
+}, false);
