04fa6b6daa4f4364702396bc339b499a2e60e150
xuefei.shi519 <xuefei.shi519@gmail.com>
2016-05-25 19:09:51 -0400

    slideshow updated

--- scripts/main.js
+++ scripts/main.js
@@ -51,15 +51,15 @@ function currentDiv(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("demo");
+  var dots = document.getElementsByClassName("dot");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length} ;
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" w3-white", "");
+     dots[i].className = dots[i].className.replace(" selected", "");
   }
   x[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " w3-white";
+  dots[slideIndex-1].className += " selected";
 }
